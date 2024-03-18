f213a2f3bc7b5b8bcd9d4b4068b05b91ae8e3f82
nichol6s <nicholas.jake15@gmail.com>
2024-03-18 20:08:22 -0300

    Button e Toast bugados mas finalizados!

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import "@/styles/global.css";
 import { StatusBar } from 'expo-status-bar';
 import { Profile } from '@/app/Profile';
+import { ToastProvider } from "@/components/Toast"
 
 export default function App() {
   return (
-    <>
+    <ToastProvider  position="top">
       <Profile />
       <StatusBar style="light" />
-    </>
+    </ToastProvider>
   );
 }
--- src/app/Profile.tsx
+++ src/app/Profile.tsx
@@ -1,27 +1,37 @@
-import { View, Image, Text } from "react-native"
+import { View, Image, ScrollView } from "react-native"
 
 import { Skills } from "@/components/Skills"
 import { User } from "@/components/User"
 import { Preferece } from "@/components/Preference"
 import { Input } from "@/components/Input"
+import { Button } from "@/components/Button"
+import { useToast } from "@/components/Toast"
 
-export function Profile(){
-    return(
+
+
+export function Profile() {
+    const { toast } = useToast()
+
+
+    return (
         <View className="flex-1 bg-gray-700">
-            <Image 
-            source={require("@/assets/banner.png")}
-            className="w-full h-52 -mb-16"
-            />
+            <ScrollView>
+                <Image
+                    source={require("@/assets/banner.png")}
+                    className="w-full h-52 -mb-16"
+                />
 
-            <View className="flex-1 px-4 pb-4">
-                <User />
-                <Skills />
-                <Preferece />
-                
-                <View className="w-full mt-6 flex-1">
-                    <Input placeholder="Company" inputClasses="mb-6" label="Company"/>
+                <View className="flex-1 px-4 pb-4">
+                    <User />
+                    <Skills />
+                    <Preferece />
+
+                    <View className="w-full mt-6 flex-1">
+                        <Input placeholder="Company" inputClasses="mb-6" label="Company" />
+                        <Button label="Save" onPress={()=> toast("Saved!")}/>
+                    </View>
                 </View>
-            </View>
+            </ScrollView>
         </View>
     )
 }
