9ef8141111535c1914b30532bab6638203a6b8e9
nichol6s <nicholas.jake15@gmail.com>
2024-03-18 16:46:27 -0300

    Configuração inicial feita com sucesso

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import {  } from "./src/styles/colors" 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./src/**/*.{ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors,
+    },
+  },
+  plugins: [],
+}
